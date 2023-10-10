2e433e543caa08686dd38e2f193fd3e32514129f
unknown <stauan621@gmail.com>
2023-10-09 21:14:01 -0400

    testte

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require ('express')
+const app = express()
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+
+app.set('view engine', 'ejs')
+app.get("/", function (request, response){
+    response.render('pages/index')
+})
+app.listen(8080)
+app.get("/sobre", function (request, response){
+    response.render('pages/about')
+})
+app.get("/contato", function (request, response){
+    response.render('pages/contact')
+})
+app.listen(8080)
+console.log('SERVER IS RUNNING');
